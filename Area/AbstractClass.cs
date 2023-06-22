9922312639f0000fb45638a70e2418e44b6556bb
Akshata <Akshata@LAPTOP-4CVBJOT7>
2023-06-22 12:51:25 +0530

    Add project files.

---
+++ Area/AbstractClass.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Area
+{
+    public abstract class Abclass
+    {
+        public abstract void CalculateArea();
+       
+    }
+    public class Rectangle:Abclass
+    {
+        public int length, breadth;
+        public int Area;
+
+        public Rectangle(int length, int breadth)
+        {
+            this.length = length;
+            this.breadth = breadth;
+        }
+        public override void CalculateArea()
+        {
+            Area = length * breadth;
+        }
+        public override string ToString()
+        {
+            return $"Area of Reactangle = {Area}";
+        }
+    }
+}
---
+++ Area/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Area
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Rectangle rectangle= new Rectangle(15,12);
+            rectangle.CalculateArea();
+            rectangle.ToString();
+           Console.WriteLine(rectangle);
+        }
+
+    }
+}
